a3638fb708228fa649aa2446e2848b85968ebe25
rodmasalskiene <rodolfowm707@gmail.com>
2024-04-12 15:18:12 -0300

    func convertListToStringVector

--- leetcode/mediumProblems/_2_AddTwoNumbers.cpp
+++ leetcode/mediumProblems/_2_AddTwoNumbers.cpp
@@ -1,15 +1,31 @@
 #include <bits/stdc++.h>
+#include <vector>
 
 using namespace std;
 
-    struct Node{
-        int val;
-        char tst;
-        struct Node *next;
-        Node(int _val) : val(_val), next(nullptr) {};
-    };
+struct Node
+{
+    int val;
+    char tst;
+    struct Node *next;
+    Node(int _val) : val(_val), next(nullptr) {};
+};
 
-int main() {
+vector<string> convertListToString(struct Node listNode)
+{
+    vector<string> listVector{};
+
+    Node *current = &listNode;
+    while(current != nullptr)
+    {
+        listVector.push_back(to_string(current->val));
+        current = current->next;
+    }
+    return listVector;
+}
+
+int main()
+{
     Node l1_node1(2);
     Node l1_node2(4);
     Node l1_node3(3);
@@ -24,13 +40,15 @@ int main() {
     l2_node1.next = &l2_node2;
     l2_node2.next = &l2_node3;
 
-//    cout << l2_node1.next->next->val;
+    vector<string> l1_vector = convertListToString(l1_node1);
+    vector<string> l2_vector = convertListToString(l2_node1);
 
-    Node *current = &l1_node1;
-    while(current != nullptr){
-        cout << current->val;
-        current = current->next;
+    for(int i = l1_vector.size(); i >= 0; i--)
+    {
+        cout << l1_vector[i];
     }
+
+
 
 //    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 //
